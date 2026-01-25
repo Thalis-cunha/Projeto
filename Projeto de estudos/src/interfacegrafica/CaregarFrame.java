65fc7e8b7e600322a223c9eedf97a8e4c78265ca
Thalis-cunha <thalishh2@gmail.com>
2026-01-25 18:22:43 -0300

    20dia

--- Projeto de estudos/src/interfacegrafica/CaregarFrame.java
+++ Projeto de estudos/src/interfacegrafica/CaregarFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class CaregarFrame {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Teste de Janelas");
+        JFrame frame = new JFrame("Testando Janelas");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(new GuiAreaDeTexto());   //altere o nome Gui... para vizualizar a parte do codigo desejada
         frame.setBounds(0, 0, 500, 300);
